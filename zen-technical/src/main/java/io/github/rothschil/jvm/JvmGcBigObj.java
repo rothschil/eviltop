5478ee70ef9fa39aec9a7a6729bedfe8a3d391eb
SamAbram <WCNGS@HOTMAIL.COM>
2022-02-09 17:36:45 +0800

    None

--- zen-technical/src/main/java/io/github/rothschil/jvm/JvmGcBigObj.java
+++ zen-technical/src/main/java/io/github/rothschil/jvm/JvmGcBigObj.java
@@ -1,23 +1,20 @@
 package io.github.rothschil.jvm;
 
-import org.openjdk.jol.info.ClassLayout;
-
-public class JvmDyncAge {
+public class JvmGcBigObj {
     public static void main(String[] args) {
         byte[] bytes1 = new byte[4 * 1024 * 1024];
         bytes1 = new byte[4 * 1024 * 1024];
         bytes1 = new byte[4 * 1024 * 1024];
-
-        bytes1 = null;
         byte[] bytes2 = new byte[128 * 1024];
+        bytes2 = null;
 
         byte[] bytes3 = new byte[4 * 1024 * 1024];
-        bytes3 = new byte[4 * 1024 * 1024];
-        bytes3 = new byte[4 * 1024 * 1024];
-        bytes3 = new byte[128 * 1024];
-
-        bytes3 = null;
-
-        byte[] bytes4 = new byte[4 * 1024 * 1024];
+//        bytes3 = new byte[4 * 1024 * 1024];
+//        bytes3 = new byte[4 * 1024 * 1024];
+//        bytes3 = new byte[128 * 1024];
+//
+//        bytes3 = null;
+//
+//        byte[] bytes4 = new byte[4 * 1024 * 1024];
     }
 }
---
+++ zen-technical/src/main/java/io/github/rothschil/jvm/JvmObj.java
@@ -0,0 +1,19 @@
+package io.github.rothschil.jvm;
+
+public class JvmObj {
+    public static void main(String[] args) {
+        byte[] bytes1 = new byte[4 * 1024 * 1024];
+        bytes1 = new byte[4 * 1024 * 1024];
+        bytes1 = new byte[4 * 1024 * 1024];
+        byte[] bytes2 = new byte[128 * 1024];
+        bytes2 = null;
+        byte[] bytes3 = new byte[4 * 1024 * 1024];
+        bytes3 = new byte[4 * 1024 * 1024];
+        bytes3 = new byte[4 * 1024 * 1024];
+        bytes3 = new byte[128 * 1024];
+        bytes3 = null;
+
+//        byte[] bytes4 = new byte[4 * 1024 * 1024];
+
+    }
+}
